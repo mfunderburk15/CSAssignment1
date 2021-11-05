5d8e7900bde7e3ebc9b8e51f1cd7c91696c6edc5
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-04 22:43:09 -0500

    no.1 solved

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -6,7 +6,14 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+              Write a program and ask the user to enter a number. The number should be between 1 to 10.
+              If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid".
+              (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+             */
+            int number = 11;
+            string legit = (number > 0 && number <= 10) ? "Valid" : "Invalid";
+            Console.WriteLine(legit);
         }
     }
 }
