24978ef07d003670193a58c7885375c09568a8a3
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-04 22:53:53 -0500

    finished 2 & 3

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -11,9 +11,36 @@ namespace Assignment1
               If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid".
               (This logic is used a lot in applications where values entered into input boxes need to be validated.)
              */
-            int number = 11;
+            int number = 5;
             string legit = (number > 0 && number <= 10) ? "Valid" : "Invalid";
             Console.WriteLine(legit);
+
+            /*
+             Write a program which takes two numbers from the console and displays the maximum of the two.
+             */
+            int number1 = 3;
+            int number2 = 3;
+            int max = (number1 > number2 || number1 == number2) ? number1 : number2;
+            Console.WriteLine(max);
+
+            /*
+                Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait. 
+             */
+            int width = 12;
+            int height = 8;
+            if (width > height)
+            {
+                Console.WriteLine("landscape");
+            }
+            else if (width < height)
+            {
+                Console.WriteLine("portrait");
+            }
+            else
+            {
+                Console.WriteLine("square image");
+            }
+
         }
     }
 }
